7d2149fbb6f2bb38f44531c39c049e58bcffda05
Dave Marshall <mail@jorviksolutions.com>
2018-11-19 11:43:53 +0000

    baseline project

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Dave Marshall on 19/11/2018.
+//  Copyright © 2018 Jorvik Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
